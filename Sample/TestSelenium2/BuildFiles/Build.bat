7360e13f27370a810d4d658dddc0a5114e140143
Thang Chung <thangchung@ymail.com>
2010-12-17 14:20:13 +0000

    + Delete some useless files
    Signed-off-by: Thang Chung <thangchung@ymail.com>

--- Sample/TestSelenium2/BuildFiles/Build.bat
+++ Sample/TestSelenium2/BuildFiles/Build.bat
@@ -1,2 +1,2 @@
-nant.exe -buildfile:D:\ThangChung\OSS\NUnit2Report_git\Sample\TestSelenium2\bin\Debug\Report\TestSelenium2.build
+D:\ThangChung\OSS\NUnit2Report_git\ExternalLibs\nant-0.91-alpha2\bin\nant.exe -buildfile:D:\ThangChung\OSS\NUnit2Report_git\Sample\TestSelenium2\bin\Debug\BuildFiles\TestSelenium2.build
 pause
